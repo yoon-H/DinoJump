e17b6eab2d61c45aacd738dc2c505db147e7ec78
yoon-H <ucando0320@gmail.com>
2024-12-20 09:04:47 +0900

    fix : cors option

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import { loadGameAssets } from './init/assets.js';
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
 
 const PORT = 3000;
 
+app.use(cors({ credentials: true, origin: 'http://gureunda.shop:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
